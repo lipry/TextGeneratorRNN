Implemented and tested (very simple test) backpropagation through time method + general bug fixing

src/Operations.py
@@ -1,3 +1,5 @@
+import warnings
+
 import numpy as np
 
 
@@ -17,7 +19,9 @@ class Multiply:
         return W.dot(x)
 
     def backward_pass(self, W, x, dz):
-        dW = np.asarray(np.dot(np.transpose(np.asarray(dz)), np.asarray(x)))
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            dW = np.asarray(np.dot(np.transpose(np.asmatrix(dz)), np.asmatrix(x)))
         dx = np.dot(np.transpose(W), dz)
         return dW, dx
 
@@ -45,4 +49,10 @@ class OutputLayer:
         return exp / exp.sum(axis=0)
 
     def loss(self, x, y):
-        pass
+        p = self.predict(x)
+        return -1*np.log(p[y])
+
+    def diff(self, x, y):
+        p = self.predict(x)
+        p[y] -= 1
+        return p

src/RNNLayer.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from src.FileReader import OneHotEncodingUtilities
-from src.Operations import Sigmoid
+from src.Operations import Sigmoid, OutputLayer
 from src.Operations import Multiply
 from src.Operations import Add
 from src.Operations import Tanh
@@ -60,10 +60,30 @@ class RnnNetwork:
             cells.append(cell)
         return cells
 
-    def back_prop(self, x, y):
-        pass
+    def backprop_through_time(self, x, y, truncated=4):
+        layers = self.forward_prop(x)
+        T = len(layers)
+        dU = np.zeros_like(self.U)
+        dW = np.zeros_like(self.W)
+        dV = np.zeros_like(self.V)
 
-
-
-
+        output = OutputLayer()
+        prev_ht = np.zeros(self.hidden_dim)
+        diff_h = np.zeros(self.hidden_dim)
+        for t in range(0, T):
+            diff_Vprod = output.diff(layers[t].Vproduct, y[t])
+            v = OneHotEncodingUtilities.one_hot_encoder(x[t], self.input_dim)
+            _, dh_prev, dUt, dWt, dVt = layers[t].backward_pass(v, prev_ht, self.U, self.W, self.V, diff_h, diff_Vprod)
+            prev_ht = layers[t].h
+            diff_Vprod = np.zeros(self.input_dim)
+            for i in range(t-1, max(t-1-truncated, -1), -1):
+                v = OneHotEncodingUtilities.one_hot_encoder(x[i], self.input_dim)
+                prev_hi = layers[i].h if i != 0 else np.zeros(self.hidden_dim)
+                _, dh_prev, dUi, dWi, dVi = layers[i].backward_pass(v, prev_hi, self.U, self.W, self.V, dh_prev, diff_Vprod)
+                dUt += dUi
+                dWt += dWi
+            dU += dUt
+            dW += dWt
+            dV += dVt
+        return dU, dW, dV
 

test/test_Operations.py
@@ -44,9 +44,8 @@ def test_multiply_backward_pass():
 
     A = np.array([[0.5, 0.8], [0.113, 0.15]])
     x = np.array([0.3, 0.2])
-    dA, dx = mul.backward_pass(A, x, [[2, 2], [2, 2]])
-    assert np.allclose(dx, [[1.226, 1.226], [1.9, 1.9]])
-    assert np.allclose(dA, [1., 1.])
+    dA, dx = mul.backward_pass(A, x, [2, 2])
+    assert np.allclose(dx, [1.226, 1.9])
 
 
 add = Add()